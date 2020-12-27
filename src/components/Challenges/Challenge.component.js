3091ec95c68191de301e88e9927915b385cf665e
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2020-12-27 08:40:27 -0600

    Add a new Challenge component and revamp the challenges tab.

---
+++ src/components/Challenges/Challenge.component.js
@@ -0,0 +1,79 @@
+import React, {useState, useEffect} from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+function computeTimeToTarget(target){
+    let date_now = new Date()
+    
+    let delta = Math.abs(target - date_now) / 1000
+
+    let days = Math.floor(delta / 86400)
+    delta -= days * 86400
+    let hours = Math.floor(delta / 3600) % 24
+    delta -= hours * 3600
+    let minutes = Math.floor(delta / 60) % 60
+    delta -= minutes * 60
+    let seconds = Math.floor(delta % 60)
+
+    return {days, hours, minutes, seconds}
+}
+
+
+const Challenge = ({title, releaseTime, desc, index, url}) => {
+
+    const [timeTo, setTimeTo] = useState(undefined);
+    let timer;
+
+    useEffect(()=>{
+        timer = setInterval(()=>{
+            let remainingTime = computeTimeToTarget(releaseTime)
+            if (isReady(remainingTime)){
+                clearInterval(timer);
+                setTimeTo({hours: '0', days: '0', minutes: '0', seconds: '0'})
+            }else{
+                setTimeTo(computeTimeToTarget(releaseTime))
+            }            
+        }, 1000);
+    }, [])
+
+    function isReady(t){
+        return t && t.days === '0' && t.hours === '0' && t.minutes === '0' && t.seconds === '0'
+    }
+
+    return (
+        <Card className="w-auto m-auto p-0 mt-2 border border-rounded">
+            <Card.Header as="h6" className="d-flex justify-content-between">
+                <span>
+                    Challenge #{index}
+                </span>
+                <div className="d-none d-md-block" style={{width: "600px"}}></div>
+                {timeTo && !isReady(timeTo) && (
+                    <span>
+                        &nbsp; &nbsp; Ready in: {`${timeTo.days.toString().padStart(2, '0')}d ${timeTo.hours.toString().padStart(2, '0')}h ${timeTo.minutes.toString().padStart(2, '0')}m ${timeTo.seconds.toString().padStart(2, '0')}s`}
+                    </span> 
+                )}
+            </Card.Header>
+            <Card.Body>
+                <Card.Title>
+                    {title}
+                </Card.Title>
+                <Card.Text>
+                {desc}
+                </Card.Text>
+                {isReady(timeTo) && (
+                   <Link to={`challenges/${url}`}>
+                       <Button variant="dark"> View Challenge </Button>
+                   </Link>
+                )}
+                {!isReady(timeTo) && (
+                    <Button variant="dark" disabled> View Challenge </Button> 
+                )}
+            </Card.Body>
+        </Card>
+    )
+}
+
+export {
+    Challenge
+}
--- src/components/Challenges/Challenges.js
+++ src/components/Challenges/Challenges.js
@@ -1,19 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { CodeForceTitleSection } from './../Miscellaneous/Miscellaneous.component'
+import { Challenge } from './Challenge.component'
+
 
 const Challenges = () => {
-    return (
-        <div className="container-fluid fullscreen pt-4 bg-dark text-warning">
-            <div className="text-center pb-4 col-sm-6 col-centered intro">
-                <h2 className="pb-4">Code Force : Challenges</h2>
-                <p>Code Force challenges are real-world programming challenges designed for teams of 3-5 people. Challenges will take anywhere from 1-3 weeks to complete.</p>
-                <p>Challenges will test knowledge in many skill areas, such as programming skills, communication and collaboration skills, and much more.</p>
-                <p>Projects will be completed using Git version control.</p>
-            </div>
 
-            <div className="col-sm-8 col-centered border border-light p-2 intro">
-                <div className="list-group text-center">
-                    <button type="button" className="list-group-item list-group-item-action disabled">Challenge #1 - Holiday Havoc - Starting December 28, 2020</button>
+    // Maybe get challenges from server. But that is for later.
+    // TODO: get challenge data from server.
+
+    const [challenges, setChallenges] = useState([
+        {
+            releaseTime: new Date("12/28/2020"),
+            title: "Holiday Havoc",
+            desc: "Some sample description goes here...",
+            url: "holidayhavoc"
+        },
+        {
+            releaseTime: new Date("12/30/2020"),
+            title: "Challenge 2",
+            desc: "Some sample description goes here...",
+            url: "holidayhavoc"
+        },
+        {
+            releaseTime: new Date("01/18/2021"),
+            title: "Challenge 3",
+            desc: "Some sample description goes here...",
+            url: "holidayhavoc"
+        }
+    ])
+
+
+    return (
+        <div className="container-fluid fullscreen intro">
+            <CodeForceTitleSection/>
+            <div className="row">
+                <div className="col-sm-9 text-center m-auto mt-4">
+                    <h1>Challenges</h1>
                 </div>
+            </div>
+            <div className="col-sm-9 m-auto p-3 mt-3">
+                <h2>In a nutshell, they ...</h2>
+                <br/>
+                    <ul className="p-3 col-sm-9">
+                        <li>are <strong>real-world programming challenges</strong> designed for teams of 3-5 people.</li>
+                        <li>are about <strong>1-3 weeks</strong> long.</li>
+                        <li><strong>test</strong> and <strong>strengthen your knowledge</strong> in many areas, such as <strong>design (both visual and system)</strong>, <strong>development (full-stack)</strong>, <strong>databases</strong>, <strong>cloud platforms</strong>, <strong>devops</strong>, and plenty more. </li>
+                        <li>help you develop soft skills like <strong> communication</strong>, <strong> team work</strong>, <strong> persistence </strong> to solve full-scale problems, etc.</li>
+                        <li>are intended for the participants to <strong>get familiar with</strong> and <strong>excited</strong> about the <strong>technologies</strong> trending in the <strong>software industry</strong>.</li>
+                    </ul>
+            </div>
+            <div className="col-sm-9 m-auto p-3 mt-3">
+                <h2>All Challenges</h2>
+                <br/>
+                <h6>(These challenges will automatically be made available on the designated date.)</h6>
+            </div>
+            <div className="row col-sm-12">
+            {[...challenges].sort((a, b) => a.releaseTime > b.releaseTime ? 1 : -1).map((item, idx) => (
+                <Challenge
+                    releaseTime={item.releaseTime}
+                    title={item.title}
+                    desc={item.desc}
+                    index={idx + 1}
+                    key={item.title}
+                    className="mb-5 mt-5"
+                    url={item.url}
+                />
+            ))}
             </div>
         </div>
     )
